8be57ceb05fcec2d890cbdd37363b8508f52c911
Manoj Sharma <ms9954098610@gmail.com>
2018-07-12 20:01:30 +0530

    Update trex.py

--- trex.py
+++ trex.py
@@ -4,35 +4,34 @@ from mss import mss
 from PIL import Image
 import pyautogui
 
-mon = {'top': 384, 'left': 220, 'width': 100, 'height': 40}#area of interest on the screen
+mon = {'top': 389, 'left': 175, 'width': 98, 'height': 40}#area of interest on the screen
 
-sct = mss()
 k=1
 while 1:
-    img=sct.grab(mon)
-    img=np.array(img)
+    img=mss().grab(mon) #capturing the screen shot
+    img=np.array(img)   #conveting the image to numpy arrays
     
-    cv2.imshow('test', img)
-    px =img[28,:,0]
-    py =img[1,:,0]
+    cv2.imshow('test', img) #just displaying the image
+    p_cac =img[28,:,0]     #storing the blue colour value of all pixels from 28th pixel 
+    p_bird =img[1,:,0]     # storing the blue colour value of all pixels from 1st pixel
     
     #print (px)
     #print (py)
-    pysum=np.sum(py)
-    pxsum=np.sum(px)
-    print (pxsum)
-    #print (pysum)
+    p_cac_sum=np.sum(p_cac)  #sum of all the bule values at 28 position
+    p_bird_sum=np.sum(p_bird)
+    print (p_cac_sum)
+    
 #white=247 and grey =83	
-    if pxsum<24700:
+    if p_cac_sum<24108:
      pyautogui.press('up')
     
-    if pysum<24700:
+    if p_bird_sum<24108:
      pyautogui.keyDown('down')
-     print (py)
-     print (pysum)
+     print (p_bird)
+     print (p_bird_sum)
      k=1	 
     
-    if pysum==24700 and k==1:
+    if p_bird_sum==24108 and k==1:
 	       pyautogui.keyUp('down')
 	       k=0
     if cv2.waitKey(25) & 0xFF == ord('q'):
